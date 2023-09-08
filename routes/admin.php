3350b2be7edc8997025af5e16bb34b9f6cee583c
mdruhulalim <mdruhulalim888888@gmail.com>
2023-09-08 23:50:37 +0600

    Slider update 0.0 from master

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 
 // admin routes
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
@@ -11,3 +12,6 @@ Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard'
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+// for Slider
+Route::resource('slider', SliderController::class);
