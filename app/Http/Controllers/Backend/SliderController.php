8621639117798dccce2909ba9236cb0f35360c74
mdruhulalim <mdruhulalim888888@gmail.com>
2023-09-12 19:21:16 +0600

    Slider update 0.1 from dev1

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Slider;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SliderController extends Controller
 {
@@ -20,7 +21,7 @@ class SliderController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.slider.create');
     }
 
     /**
@@ -28,7 +29,28 @@ class SliderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            // 'banner'    =>   ['required', 'image', 'max:2000'],
+            'type'  =>  ['string', 'max:200'],
+            'title' =>  ['required', 'max:200'],
+            'starting_price'    =>  ['max:200'],
+            'btn_url'   =>  ['url'],
+            'serial'    =>  ['required', 'integer'],
+            'status'    =>  ['required']
+        ]);
+        $slider = new Slider();
+
+        $slider->type   =   $request->type;
+        $slider->title   =   $request->title;
+        $slider->starting_price   =   $request->starting_price;
+        $slider->btn_url   =   $request->btn_url;
+        $slider->serial   =   $request->serial;
+        $slider->status   =   $request->status;
+        $slider->save();
+
+        toastr('Slider created successfully!', 'success');
+
+        return redirect()->back();
     }
 
     /**
